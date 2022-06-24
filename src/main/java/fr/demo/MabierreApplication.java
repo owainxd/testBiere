d1cbffd811d0e522d697e5185e54c0971c58cfcf
Owainxd <mermozrpz@gmail.com>
2022-06-24 15:18:58 +0200

    my first commit

---
+++ src/main/java/fr/demo/MabierreApplication.java
@@ -0,0 +1,13 @@
+package fr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabierreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabierreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fr/demo/entities/Biere.java
@@ -0,0 +1,5 @@
+package fr.demo.entities;
+
+public class Biere {
+
+}
